c07a3b377fbf85606332fbe1508cff475aefb499
gordoneliel <gordoneliel@gmail.com>
2015-11-13 15:47:42 -0800

    Initial Commit

---
+++ LinearProgressBar/LinearProgressBar.h
@@ -0,0 +1,19 @@
+//
+//  LinearProgressBar.h
+//  LinearProgressBar
+//
+//  Created by Eliel Gordon on 11/13/15.
+//  Copyright © 2015 Eliel Gordon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LinearProgressBar.
+FOUNDATION_EXPORT double LinearProgressBarVersionNumber;
+
+//! Project version string for LinearProgressBar.
+FOUNDATION_EXPORT const unsigned char LinearProgressBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LinearProgressBar/PublicHeader.h>
+
+
